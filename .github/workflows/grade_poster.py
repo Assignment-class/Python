603011912c98d7c4a730c3b12916caff910b4744
DhaniDS <dhanidwinawans12@gmail.com>
2025-06-22 21:18:27 +0700

    Update grade_poster.py

--- .github/workflows/grade_poster.py
+++ .github/workflows/grade_poster.py
@@ -14,7 +14,7 @@ ASSIGNMENT_ID = 4  # Sebaiknya gunakan angka, bukan string
 # 2. Pemetaan Manual dari Username GitHub ke Email yang Terdaftar di Moodle
 GITHUB_TO_EMAIL_MAP = {
     "DhaniDS": "fastgoole@gmail.com",
-    "DhaniDS": "dhanidwinawans12@gmail.com",
+    "dhanidds": "dhanidwinawans12@gmail.com",
     # --- TAMBAHKAN SEMUA MAHASISWA ANDA DI SINI ---
 }
 
