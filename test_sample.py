2193247d3d9d47ad2dd71bde8a49b7ef40705c80
dhanidds <tonodds@gmail.com>
2025-06-22 21:17:30 +0700

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,3 @@
 def test_selalu_lulus():
     """Tes ini tidak melakukan apa-apa dan dijamin akan selalu lulus."""
     assert 1 == 1
-
-def test_penjumlahan_sederhana():
-    """Tes ini memeriksa operasi penjumlahan sederhana."""
-    assert 2 + 2 == 4
